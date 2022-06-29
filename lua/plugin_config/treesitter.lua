e446d083492f7cc7348d83c85541e32e54e0c91e
nalleyer <nalleyer@gmail.com>
2022-06-29 21:24:06 +0800

    add glsl tree-sitter

--- lua/plugin_config/treesitter.lua
+++ lua/plugin_config/treesitter.lua
@@ -3,7 +3,7 @@ function M.init()
     -- nvim-treesitter config
     require'nvim-treesitter.configs'.setup {
         -- ensure_installed = "maintained", -- for installing all maintained parsers
-        ensure_installed = { "rust", "lua", "python", "proto", "c_sharp" }, -- for installing specific parsers
+        ensure_installed = { "rust", "lua", "python", "proto", "c_sharp", "glsl" }, -- for installing specific parsers
         sync_install = true, -- install synchronously
         ignore_install = { }, -- parsers to not install
         highlight = {
