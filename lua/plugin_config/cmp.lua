153ecfe9d0c2a868451dd229e32c59db47ecc6d7
nalleyer <nalleyer@gmail.com>
2022-08-15 23:21:04 +0800

    use clangd, delete glsl

--- lua/plugin_config/cmp.lua
+++ lua/plugin_config/cmp.lua
@@ -136,7 +136,7 @@ cmp.setup.cmdline(':', {
 
 -- Setup lspconfig.
 require 'lspconfig'.gopls.setup {}
-local servers = { 'ccls', 'html', 'tsserver', 'rust_analyzer', 'bashls', 'pyright', 'gopls', 'sumneko_lua', 'racket_langserver' }
+local servers = { 'clangd', 'html', 'tsserver', 'rust_analyzer', 'bashls', 'pyright', 'gopls', 'sumneko_lua', 'racket_langserver' }
 local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
 -- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
 for _, lsp_name in ipairs(servers) do
--- lua/plugin_config/treesitter.lua
+++ lua/plugin_config/treesitter.lua
@@ -1,9 +1,10 @@
 local M = {}
 function M.init()
+    require 'nvim-treesitter.install'.compilers = { "clang", "gcc" }
     -- nvim-treesitter config
     require'nvim-treesitter.configs'.setup {
         -- ensure_installed = "maintained", -- for installing all maintained parsers
-        ensure_installed = { "rust", "lua", "python", "proto", "c_sharp", "glsl" }, -- for installing specific parsers
+        ensure_installed = { "rust", "lua", "python", "proto", "c_sharp" }, -- for installing specific parsers
         sync_install = true, -- install synchronously
         ignore_install = { }, -- parsers to not install
         highlight = {
