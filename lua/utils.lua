610659e91a7c4535115dd55c1700db9a5b8b6931
nalleyer <nalleyer@gmail.com>
2023-07-31 10:59:33 +0800

    utils: add print_r function

--- lua/utils.lua
+++ lua/utils.lua
@@ -30,4 +30,56 @@ function u.is_big_file()
   return line_count > 100000
 end
 
+
+local tconcat = table.concat
+local tinsert = table.insert
+local srep = string.rep
+local INDENT = "    "
+
+function u.print_r_dump(root)
+    local cache = { [root] = "." }
+    local function _dump(t, indent, name)
+        indent = indent or ""
+        name = name or ""
+
+        local temp = {}
+        if t == nil then
+            tinsert(temp, indent .. "nil")
+        elseif type(t) == "table" then
+            local next_indent = indent .. INDENT
+            tinsert(temp, "{")
+
+            for k, v in pairs(t) do
+                local key = tostring(k)
+
+                if cache[v] then
+                    tinsert(temp, next_indent .. "+" .. key .. "=>{" .. cache[v] .. "}")
+                else
+                    local new_key = name .. "." .. key
+                    if type(v) == "table" then
+                        cache[v] = new_key
+                    end
+                    tinsert(temp, next_indent .. "+" .. key .. _dump(v, next_indent, new_key))
+                end
+            end
+
+            tinsert(temp, indent .. "}")
+        else
+            tinsert(temp, ":" .. tostring(t) .. "")
+        end
+
+        return tconcat(temp, "\n")
+    end
+
+	return _dump(root)
+end
+
+function u.print_r(root)
+    if __RELEASE__ then
+        return
+    end
+
+   print(u.print_r_dump(root))
+end
+
 return u
